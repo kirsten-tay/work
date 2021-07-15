f535c8eefddd77372a213128916dad645523a808
kirsten tay <taykirsten11@gmail.com>
2021-07-15 20:34:31 +0000

    Updated the Navbar. Added a logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from './../Assets/logo.jpg'
 const Navbar=() => {
     return (
      
-          <div className=" flex flex-row flex justify-between items-center bg-red-600 px-10 border-b border-gray-200">
+          <div className=" flex flex-row flex justify-between items-center bg-white-600 px-10 border-b border-gray-200">
             <div className="flex flex-1 justify-start">
               <img src={logo} className="h-20 w-20" />
                           <div className="flex text-2xl py-5"><span>PIZZAMANIA</span></div>
