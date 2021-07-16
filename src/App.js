4047b1e5a2d126caacee2c6b53e6fe1a127862f1
kirsten tay <taykirsten11@gmail.com>
2021-07-16 08:29:45 +0000

    Added a footer and carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import react from 'react';
+
+
+const Footer =()=>{
+    return(
+        <div class="space-y-4">
+  <div class="flow-root bg-blue-600 ...">
+    <div class="my-4 ...">
+         <div className="flex flex-col justify-center">
+            <span className="text-bold-2xl">VISIT US IN KENYA</span>
+                 <span>Nairobi,Kenya.</span> 
+ 
+        <span>Belgravia Centre,14 Riverside drive,
+          4th floor, Off Riverside Drive.
+           Hanover Centre, 14 Riverside Drive,
+            6th floor, Off Riverside Drive 
+</span> 
+         </div>
+        </div>
+  </div>
+
+</div>
+    )
+}
+
+
+export default Footer;
