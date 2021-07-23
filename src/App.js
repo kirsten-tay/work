95f4d18cb99f84349c9d63625ccc22c5083767bc
kirsten tay <taykirsten11@gmail.com>
2021-07-23 21:44:23 +0000

    Updated the Carousel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Carousel from './components/Carousel'
 import Hero from './components/Hero'
+import Slider from './components/Slider'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Slider/>
         <Carousel/>
         <Hero/>
         <Footer/>
