17e09cce18d3a18d894a625aa100af9e9413977e
kirsten tay <taykirsten11@gmail.com>
2021-08-01 10:05:20 +0000

    Updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,36 @@
-import react from 'react';
+import react,{Fragment} from 'react';
 
 
 const Footer =()=>{
     return(
-        <div class="space-y-4">
-  <div class="flow-root bg-blue-600 ...">
-    <div class="my-4 ...">
-         <div className="flex flex-col justify-center">
-            <span className="text-bold-2xl">VISIT US IN KENYA</span>
+    <Fragment>
+    
+        <div className="flex flex-row ..flow-root bg-blue-600 ...">
+        <div className="flex flex-col justify-center m-5  items-center ">
+            <span>VISIT US IN KENYA</span>
                  <span>Nairobi,Kenya.</span> 
- 
         <span>Belgravia Centre,14 Riverside drive,</span> 
           <span>4th floor, Off Riverside Drive.</span>
            <span>Hanover Centre, 14 Riverside Drive,</span>
            <span> 6th floor, Off Riverside Drive </span>
 
          </div>
-         <div className="flex flex-col justify-center">
-         <span className="text-bold-2xl"></span>
+         <div className=" flex flex-col justify-center m-5 items-center">
+         <span ></span>
          <span>Accra, Ghana.</span>
           <span>
             One Airport Square,Airport City</span>
         <span>  8th floor..
          </span>
          </div>
-         <div className="flex flex-col justify-center">
-         <span className="text-bold-2xl">VISIT US IN NIGERIA</span>
+         <div className="flex flex-col justify-center m-5  items-center">
+         <span >VISIT US IN NIGERIA</span>
           <span> Lagos,Nigeria.</span>
 
          <span> Sterling Bank Building.</span>
          </div>
         </div>
-  </div>
-
-</div>
+ </Fragment>
     )
 }
 
